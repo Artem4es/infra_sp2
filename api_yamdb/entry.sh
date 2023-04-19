a65adf9c16dcf08c625dfc2c5624abfbd23c4b9a
artem4es <dragster1001@yandex.ru>
2023-04-19 14:30:15 +0300

    -homework 1.5

---
+++ api_yamdb/entry.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+gunicorn api_yamdb.wsgi:application --bind 0:8000
